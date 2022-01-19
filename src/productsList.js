692cd6cea178e4bc1858f2641dea619b5f71efdb
Thomas F <thomasfran1935@gmail.com>
2022-01-19 13:31:13 +0100

    done

---
+++ src/productsList.js
@@ -0,0 +1,42 @@
+const items = [
+  {
+    id: 1,
+    name: "Mètre",
+    utility: "baignoire",
+    image: "https://i.imgur.com/cUzX22l.jpg",
+    desc: "Mesure mètre à ruban Stanley Tylon bi-matière 5 mètres x 19 mm classe 2",
+    price: 6.50,
+  },
+  {
+    id: 2,
+    name: "Niveau à bulle",
+    utility: "baignoire",
+    image: "https://i.imgur.com/HtYneyC.png",
+    desc: "Longueur : 400 mm",
+    price: 20,
+  },
+  {
+    id: 3,
+    name: "Règle de maçon",
+    utility: "baignoire",
+    image: "https://i.imgur.com/W7dT9zB.jpg",
+    desc: "2 voiles (3 alvéoles). Section 100 x 18 mm. Epaisseur 1 mm +/- 0,05. Longueur : 6m.",
+    price: 30.50,
+  },
+  {
+    id: 4,
+    name: "Clef à molette",
+    utility: "baignoire",
+    image: "https://i.imgur.com/QfLWbqM.jpg",
+    desc: "Clé à molette 30 pouces Clé à molette, clé à fourche large, forgée, chromée, outils à main, 775 MM",
+    price: 328,
+  },
+  {
+    id: 5,
+    name: "Clef plate",
+    utility: "baignoire",
+    image: "https://i.imgur.com/gJjGbzH.jpg",
+    desc: "Clé plate Facom 20 X 22 mm",
+    price: 9.05,
+  }
+]
