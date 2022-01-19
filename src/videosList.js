31416a21aec329f2f534bf7145335535c45efbfa
Aphideth <aphideth.dev@gmail.com>
2022-01-19 20:26:50 +0100

    add databse workers and tutos

---
+++ src/videosList.js
@@ -0,0 +1,54 @@
+const videos = [
+  {
+    id: 1,
+    name: "Astuce1",
+    url: "https://www.youtube.com/watch?v=72TCBR9CFjQ&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=1",
+  },
+  {
+    id: 2,
+    name: "Astuce2",
+    url: "https://www.youtube.com/watch?v=ldSCQS_Yngw&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=2",
+  },
+  {
+    id: 3,
+    name: "Astuce3",
+    url: "https://www.youtube.com/watch?v=JayrGuPywTI&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=3",
+  },
+  {
+    id: 4,
+    name: "Astuce4",
+    url: "https://www.youtube.com/watch?v=HIURSQPRcO0&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=4",
+  },
+  {
+    id: 5,
+    name: "Astuce5",
+    url: "https://www.youtube.com/watch?v=I6Ww5blG-5M&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=5",
+  },
+  {
+    id: 6,
+    name: "Astuce6",
+    url: "https://www.youtube.com/watch?v=lomMIbmjC7w&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=6",
+  },
+  {
+    id: 7,
+    name: "Astuce7",
+    url: "https://www.youtube.com/watch?v=wIv7_msg32Y&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=7",
+  },
+  {
+    id: 8,
+    name: "Astuce8",
+    url: "https://www.youtube.com/watch?v=vnLckEmMP8Q&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=8",
+  },
+  {
+    id: 9,
+    name: "Astuce9",
+    url: "https://www.youtube.com/watch?v=-AbTYVDPmAA&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=9",
+  },
+  {
+    id: 10,
+    name: "Astuce10",
+    url: "https://www.youtube.com/watch?v=OuIARDznbFo&list=PLMkemzexwwbRInnwIBnllYfZJtYtsYsgN&index=10",
+  },
+];
+
+export default videos;
---
+++ src/workersList.jsx
@@ -0,0 +1,76 @@
+const workers = [
+  {
+    id: 1,
+    name: "Vincent",
+    img: "https://i.imgur.com/5HMR5Eg.jpg",
+    speciality: "plombier",
+  },
+  {
+    id: 2,
+    name: "Hubert",
+    img: "https://i.imgur.com/iYkuRf5.jpg",
+    speciality: "macon",
+  },
+  {
+    id: 3,
+    name: "Pedro",
+    img: "https://i.imgur.com/WR2oZkS.jpg",
+    speciality: "electricien",
+  },
+  {
+    id: 4,
+    name: "Loïc",
+    img: "https://i.imgur.com/rbB8HlT.jpg",
+    speciality: "menuisier",
+  },
+  {
+    id: 5,
+    name: "Jean",
+    img: "https://i.imgur.com/M2a8oAT.jpg",
+    speciality: "peintre",
+  },
+  {
+    id: 6,
+    name: "Roberto",
+    img: "https://i.imgur.com/qAtEGg2.jpg",
+    speciality: "peintre",
+  },
+  {
+    id: 7,
+    name: "Adrien",
+    img: "https://i.imgur.com/4LsV9qN.jpg",
+    speciality: "electricien",
+  },
+  {
+    id: 8,
+    name: "Arthur",
+    img: "https://i.imgur.com/ogUing0.jpg",
+    speciality: "plombier",
+  },
+  {
+    id: 9,
+    name: "Paulo",
+    img: "https://i.imgur.com/n2HmJ96.jpg",
+    speciality: "electricien",
+  },
+  {
+    id: 10,
+    name: "Clément",
+    img: "https://i.imgur.com/VVryK28.jpg",
+    speciality: "peintre",
+  },
+  {
+    id: 11,
+    name: "Louis",
+    img: "https://i.imgur.com/SW4Jp3Y.jpg",
+    speciality: "macon",
+  },
+  {
+    id: 12,
+    name: "Jérémy",
+    img: "https://i.imgur.com/rJfrws2.jpg",
+    speciality: "plombier",
+  },
+];
+
+export default workers;
